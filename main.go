ed6b3fc36d08e03d21c30438b458b137ddda7440
gmwinston <gmwinston@gmail.com>
2022-04-16 22:06:07 +0800

    3nd

--- main.go
+++ main.go
@@ -13,5 +13,7 @@ func main() {
 }
 
 func handleFunc(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(writer, "<h1>Hello, goblog</h1>")
+	writer.Write([]byte("path:" + request.URL.Path + "; "+ request.Method))
 }
